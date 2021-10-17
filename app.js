5022dbd328daf7acc58f6d993b8780c47e40ae7d
Mariela Portillo <ruthmarielaorellana1998@gmail.com>
2021-10-17 13:07:06 -0600

    Funcional

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 
 import  Express  from "express";
+
+import Opinion from './models/opinion.js';
 import Morgan from "morgan";
 import Rutasopinion from "./routes/opinion.routes.js";
 //mport RutasUsuario from   "./routes/usuario.routes.js";
 import swaggerUI from "swagger-ui-express";
 import swaggerJSDoc from "swagger-jsdoc";
 import {createValoracion} from "./libs/configInit.js";
+import fetch from 'node-fetch'
 
 
 createValoracion();
@@ -41,8 +44,53 @@ app.use(Rutasopinion)
 app.listen(process.env.PORT || 4000)
 
 //GET con  
-app.get("/",(req,res)=>{
-    res.send("hola mongo");
+app.get("/",async(req,res)=>{
+    
+  await fetch("https://api-comercios.herokuapp.com/puntuacion")
+  .then((resp) => {
+      return resp.json()
+  }).then((res) => {
+      console.log(JSON.stringify(res))
+      let menor = res.menor;
+      let mayor = res.mayor;
+      let promedio = res.promedio;
+      console.log(menor)
+      console.log(mayor)
+      console.log(promedio)
+      if ((menor < mayor) && (promedio <= mayor) && (promedio >= menor)) {
+          new Opinion({
+              comentario: "Los productos son buenos, recomiendo el comercio",
+              valoracion: mayor
+          }).save()
+          
+
+      } else {
+          if ((menor < mayor) && (promedio >= mayor)) {
+              new Opinion({
+                  comentario: "Los productos del comercio son buenos",
+                  valoracion: promedio
+              }).save()
+              
+          }
+          else {
+              if ((menor < mayor) && (promedio === menor)) {
+                  new Opinion({
+                      comentario: "Malisimo, mal servicio no lo recomiendo para nada",
+                      valoracion: menor
+                  }).save()
+                  
+
+
+              }
+          }
+      }
+  })
+
+  const opinion = await Opinion.find().sort({ $natural:-1 }).limit(1)
+  res.send(opinion)
+  
+  
+  //res.send("hola mongo");
 }) 
 
 
